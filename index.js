0a3b56554c89f9288388e29afeb68985797f03b6
maharajothit <58501144+maharajothit@users.noreply.github.com>
2022-06-28 10:59:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const clients = [{'id': 1, 'name':'DocomoPacific', 'deviceCount': 10},
+    {'id': 2, 'name':'Airtel', 'deviceCount': 12},
+    {'id': 3, 'name':'Samsung', 'deviceCount': 14}]
+
+app.get("/", function(req, res) {
+        res.send({data:clients});
+  });
+app.get("/myval", function(req, res) {
+    const id = req.query.id;
+    console.log("------", id)
+    const client = clients.find(x => x.id == id);
+    res.send({data:client});
+});
+
+app.post("/", function(req, res) {
+    res.send("This is a post request!!\n");
+  });
+        
+
+const port = 9000
+app.listen(port,() => {
+    console.log("Hi Jo Server started")
+})
+
+
